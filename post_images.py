d6e25bfac6ca21e3c3a78bb36c403a2f3149a065
gomazarashi <Seto42357@gmail.com>
2024-03-14 22:20:43 +0900

    add: チャンネル内で「!image」と投稿した際、botがimageフォルダ内の画像を投稿する機能を追加

--- post_images.py
+++ post_images.py
@@ -2,6 +2,7 @@ import os
 from os.path import join, dirname  # ファイルのパスを取得するために使用
 from dotenv import load_dotenv  # 環境変数の読み込みに使用
 import discord
+from get_images_list import get_images_list
 
 # .envファイルのパスを指定
 dotenv_path = join(dirname(__file__), ".env")
@@ -9,8 +10,10 @@ dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
 BOT_TOKEN = os.environ.get("TOKEN")
-
-discord_client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.typing = False
+intents.message_content = True
+discord_client = discord.Client(intents=intents)
 
 CHANNEL_ID = 1217780403327467611  # 画像投稿テスト用のチャンネルID
 
@@ -25,6 +28,18 @@ async def send_message():
 @discord_client.event
 async def on_ready():
     await send_message()
+
+
+# チャンネル内で「!image」と発言した際に実行されるイベント
+@discord_client.event
+async def on_message(message):
+    channel = message.channel
+    if message.author.bot:
+        return
+    if message.content == "!image":
+        images_list = get_images_list()
+        for image in images_list:
+            await channel.send(file=discord.File(f"images/{image}"))
 
 
 # botを起動
