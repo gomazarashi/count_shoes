4e9d7e6a6da29cd0963d2fb1bab20b8d4d0b1885
gomazarashi <Seto42357@gmail.com>
2024-03-23 01:36:52 +0900

    change: discordwebhookを利用して送信するよう変更

--- get_post_images_wh.py
+++ get_post_images_wh.py
@@ -1,5 +1,32 @@
 import os
 from os.path import join, dirname
 from get_images_list import get_images_list
+from post_webhook import post_text_wh, post_image_wh
+from get_image import get_image
+import shutil
+from time import sleep
+
+def move_image(image):
+    try:
+        shutil.move(f"saved_images/{image}", f"posted_images/{image}")
+    except Exception as e:
+        print(f"画像の移動中にエラーが発生しました: {e}")
+        
+
+def main():
+    post_text_wh("メッセージの送信を開始します")
+    get_image()
+    sleep(5)
+    images_list=get_images_list("saved_images")
+    for image in images_list:
+        post_image_wh(f"saved_images/{image}")
+        sleep(5)
+        move_image(image)
+    post_text_wh("メッセージの送信を終了します")
+
+        
+if __name__ == "__main__":
+    main()
+    
 
 
--- post_webhook.py
+++ post_webhook.py
@@ -2,43 +2,30 @@
 from dotenv import load_dotenv  # 環境変数を読み込むためのライブラリ
 import os
 from os.path import join, dirname  # ファイルのパスを扱うためのライブラリ
-import requests  # HTTPリクエストを送信するためのライブラリ
-import json  # JSONを扱うためのライブラリ
-
+from discordwebhook import Discord
 
 dotenv_path = join(dirname(__file__), ".env")  # 環境変数を読み込む
 load_dotenv(dotenv_path)
 
 WEBHOOK_URL = os.environ.get("WEBHOOK_URL")  # 環境変数からWEBHOOK_URLを取得
 
+discord = Discord(url=WEBHOOK_URL)
+
 def post_text_wh(content="メッセージ送信テスト"):
-    main_content = {"username": "bot", "content": content}
-
-    headers = {"Content-Type": "application/json"}  # ヘッダーの設定
-
-    response = requests.post(
-        WEBHOOK_URL, json.dumps(main_content), headers=headers
-    )  # メッセージを送信
+    discord.post(content=content)
     return
 
-def post_image_wh(image_path,content="画像を送信します"):
-    embeds = [
-        {"image": {"url": image_path}},
-    ]
-
-    main_content = {"username": "bot", 
-                    "content": content,
-                    "embeds": embeds}
-
-    headers = {"Content-Type": "application/json"}  # ヘッダーの設定
-
-    response = requests.post(
-        WEBHOOK_URL, json.dumps(main_content), headers=headers
-    )  # メッセージを送信
+def post_image_wh(image_path):
+    discord.post(
+    file={
+        "file1": open(image_path, "rb"),
+    },
+)
     return
 
 def main():
-    image_path = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Cat_poster_1.jpg"
+    image_path = "./posted_images/20240323_005544.jpg"
+    print(image_path)
     post_image_wh(image_path)
     
 if __name__ == "__main__":
