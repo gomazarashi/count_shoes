5bcc14122e27cfb81e32b30de15de54a342fd342
gomazarashi <Seto42357@gmail.com>
2024-04-18 16:29:39 +0900

    chang: scheduleを使用している箇所の削除と変更
    cronを利用して定期実行を行うため

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from my_package.get_image import get_image
 from my_package.post_webhook import post_text_wh, post_image_wh
 import shutil  # ファイル移動用
 from time import sleep  # スリープ用
-import datetime  # 現在時刻取得用
-import schedule  # スケジュール実行用
+from datetime import datetime  # 現在時刻取得用
 from PIL import Image
+
 
 def move_image(from_folder, to_folder, image):
     try:
@@ -15,9 +15,16 @@ def move_image(from_folder, to_folder, image):
         print(f"画像の移動中にエラーが発生しました: {e}")
 
 
+def post_current_time():
+    current_time = datetime.now().strftime("%H:%M:%S")
+    post_text_wh(f"現在の時刻は{current_time}です")
+
+
 def post_images():
     get_image("saved_images")
     images_list = get_filename_list("saved_images")
+    post_current_time()
+
     for image in images_list:
         post_image_wh("saved_images", image)
         sleep(1)
@@ -25,74 +32,32 @@ def post_images():
 
 
 def start_job():
-    post_text_wh(
-        f"現在の時刻は{datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")}です"
-    )
     post_text_wh("画像の送信を開始します")
-    main_job()
-    schedule.every(5).minutes.do(main_job)
+    post_images()
 
 
 def end_job():
-    post_text_wh(
-        f"現在の時刻は{datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")}です"
-    )
     post_text_wh("画像の送信を終了します")
-    for job in schedule.jobs:
-        if "main_job" in str(job):
-            schedule.cancel_job(job)
-            break
     # posted_imagesフォルダの画像を削除
     shutil.rmtree("posted_images")
     os.mkdir("posted_images")
 
 
-# 初回起動時に実行
-def first_time():
-    print(f"現在時刻: {datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')}")
-    print("「分」の値が5の倍数になるまで待機します。")
-    while True:
-        now = datetime.datetime.now()
-        if now.minute % 1 == 0 and now.second == 0:
-            print("これ以降、5分毎にメッセージを送信します。")
-            start_job()
-            break
-        sleep(1)
-
-
-def main_job():
-    post_images()
-
-
 def main():
-    first_time()
-
-    # 月曜日～土曜日の8:30～22:00の間に実行
-    schedule.every().monday.at("08:30").do(start_job)
-    schedule.every().monday.at("22:00").do(end_job)
-
-    schedule.every().tuesday.at("08:30").do(start_job)
-    schedule.every().tuesday.at("22:00").do(end_job)
-
-    schedule.every().wednesday.at("08:30").do(start_job)
-    schedule.every().wednesday.at("22:00").do(end_job)
-
-    schedule.every().thursday.at("08:30").do(start_job)
-    schedule.every().thursday.at("22:00").do(end_job)
-
-    schedule.every().friday.at("08:30").do(start_job)
-    schedule.every().friday.at("22:00").do(end_job)
-
-    schedule.every().saturday.at("08:30").do(start_job)
-    schedule.every().saturday.at("22:00").do(end_job)
-
-    # 日曜日の9:30～18:00の間に実行
-    schedule.every().sunday.at("09:30").do(start_job)
-    schedule.every().sunday.at("18:00").do(end_job)
-
-    while True:
-        schedule.run_pending()
-        sleep(1)
+    now = datetime.now()
+    # 月曜日～土曜日の8:30あるいは日曜日の9:30に実行(画像送信開始)
+    if ((now.weekday() < 6) and (now.hour == 8 and now.minute == 30)) or (
+        (now.weekday() == 6) and (now.hour == 9 and now.minute == 30)
+    ):
+        start_job()
+    # 月曜日～土曜日の22:00あるいは日曜日の18:00に実行(画像送信終了)
+    elif ((now.weekday() < 6) and (now.hour == 22 and now.minute == 0)) or (
+        (now.weekday() == 6) and (now.hour == 18 and now.minute == 0)
+    ):
+        end_job()
+    # それ以外の時間は通常の画像送信
+    else:
+        post_images()
 
 
 if __name__ == "__main__":
