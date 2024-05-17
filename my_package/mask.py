d4063baa2256fe69d1550cd10a4ef47a4953412e
gomazarashi <Seto42357@gmail.com>
2024-05-17 09:14:15 +0900

    clean: コメントを追加

--- my_package/mask.py
+++ my_package/mask.py
@@ -3,10 +3,12 @@ import cv2
 import matplotlib as plt
 
 def mask(img_path:str,mask_path:str)->str:
-    # 画像をグレースケールに変換
+    #画像の読み込み
     img=cv2.imread(img_path)
     mask=cv2.imread(mask_path,0)
+    #mask処理
     img_masked=cv2.bitwise_and(img,img,mask=mask)
+    #画像の保存
     cv2.imwrite(f'{img_path.removesuffix(".png")}_masked.png',img_masked)
     
 if __name__ == '__main__':
