1aaf7b11e982b751254b8225559be6751dde852a
gomazarashi <Seto42357@gmail.com>
2024-05-20 11:28:56 +0900

    add:count_shoes関数を完成

--- my_package/count_shoes.py
+++ my_package/count_shoes.py
@@ -1,9 +1,8 @@
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 
 
-def count_shoes(path: str) -> int:
+def count_shoes(path: str) -> bool:
     # 画像を読み込む
     img = cv2.imread(path)
     # 画像をグレースケールに変換
@@ -20,7 +19,7 @@ def count_shoes(path: str) -> int:
     )
 
     # オープニング処理の回数を設定
-    iterations = 1
+    iterations = 2
 
     # カーネル
     kernel = np.array(
@@ -41,30 +40,29 @@ def count_shoes(path: str) -> int:
     img_opening = cv2.bitwise_not(img_opening)
 
     # 輪郭抽出
-    contours, hierarchy = cv2.findContours(
-        img_opening, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE
+    contours, _ = cv2.findContours(
+        img_opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
     )
 
-    # 一定以上の面積の輪郭のみ残す
-    area = 1000
-    contours = [cnt for cnt in contours if cv2.contourArea(cnt) > area]
+    # 一定範囲の面積の輪郭のみを抽出
+    min_area = 25000
+    contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_area]
 
-    # 輪郭描画
-    img_contour = cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
+    max_area = 100000
+    contours = [cnt for cnt in contours if cv2.contourArea(cnt) < max_area]
+
 
     # 二値化画像、オープニング画像、輪郭画像を表示
-    plt.figure(figsize=(18, 4))
-    plt.subplot(1, 3, 1)
-    plt.imshow(cv2.cvtColor(img_canny, cv2.COLOR_BGR2RGB))
-    plt.title("Binary")
-    plt.subplot(1, 3, 2)
-    plt.imshow(cv2.cvtColor(img_opening, cv2.COLOR_BGR2RGB))
-    plt.title(f"オープニング処理\n回数: {iterations}")
-    plt.subplot(1, 3, 3)
-    plt.imshow(cv2.cvtColor(img_contour, cv2.COLOR_BGR2RGB))
-    plt.title(f"Contour\n物体の数:{len(contours)}")
-    plt.show()
+    if len(contours) > 0:
+        # 靴がある場合
+        return True
+    else:
+        # 靴がない場合
+        return False
 
 
 if __name__ == "__main__":
-    count_shoes("shoes_cut.jpg")
+    if count_shoes("shoes.png"):
+        print("部室に人がいます")
+    else:
+        print("部室に人はいません")
