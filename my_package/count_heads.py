018f8e8f1d2a46d926a35e71d61eab44448e01b7
gomazarashi <Seto42357@gmail.com>
2024-08-19 22:09:28 +0900

    add: 一旦count_heads関数を完成

--- my_package/count_heads.py
+++ my_package/count_heads.py
@@ -1,13 +1,19 @@
 from ultralytics import YOLO
+import math
+import sys
 
 
 def count_heads(image_path: str) -> int:
     model = YOLO("./best.pt")
     results = model(image_path)
-    head_count = len(results[0].boxes)
+    shoes_count = len(results[0].boxes)
+    # 1人が2足の靴を持っていると仮定 端数切り上げ
+    head_count = math.ceil(shoes_count / 2)
     return head_count
 
 
 if __name__ == "__main__":
-    head_count = count_heads("shoes.jpg")
+    # コマンドライン引数から画像ファイルパスを取得
+    file_path = sys.argv[1]
+    head_count = count_heads(file_path)
     print(f"部室にいるのは{head_count}人です")
